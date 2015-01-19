970014b78879409fbbfa5115760d0b823ca499a2
Renato Camilio <renatomcamilio@gmail.com>
2015-01-18 17:23:50 -0800

    Initial Commit

---
+++ TwoPlayerMathGame/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  TwoPlayerMathGame
+//
+//  Created by Renato Camilio on 1/18/15.
+//  Copyright (c) 2015 Renato Camilio. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
